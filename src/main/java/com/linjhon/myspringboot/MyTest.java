0db3682e0845c553ad2b26d5a4a54d6fbbb8eb91
linjhonfang <linjhonfang@163.com>
2019-11-03 22:32:15 +0800

    spring cloud configuration

--- src/main/java/com/linjhon/myspringboot/MyTest.java
+++ src/main/java/com/linjhon/myspringboot/MyTest.java
@@ -2,12 +2,16 @@ package com.linjhon.myspringboot;
 
 import com.linjhon.myspringboot.service.UserService;
 import com.linjhon.myspringboot.service.impl.UserServcieImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyTest {
+   private static Logger logger = LoggerFactory.getLogger(MyTest.class);
     public static void main(String[] args) {
         System.out.println("write in springboot-1221");
         System.out.println("write in springboot-20190303");
         UserService userService = new UserServcieImpl();
+        logger.debug("finish");
     }
 
 }
---
+++ src/main/java/com/linjhon/myspringboot/consul/ConsulApplication.java
@@ -0,0 +1,24 @@
+package com.linjhon.myspringboot.consul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ConsulApplication {
+    Logger logger = LoggerFactory.getLogger(ConsulApplication.class);
+    @GetMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulApplication.class);
+    }
+}
