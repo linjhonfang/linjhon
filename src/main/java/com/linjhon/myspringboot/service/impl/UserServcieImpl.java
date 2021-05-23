3612d33aa787b8df51c097fe9174f310a1edfe40
linjhonfang <linjhonfang@163.com>
2021-05-23 12:32:41 +0800

    revert with conflit

--- src/main/java/com/linjhon/myspringboot/service/impl/UserServcieImpl.java
+++ src/main/java/com/linjhon/myspringboot/service/impl/UserServcieImpl.java
@@ -6,10 +6,6 @@ public class UserServcieImpl implements UserService{
 
     @Override
     public String getAllUsers() {
-<<<<<<< HEAD
-        return "{\"name\":\"linjhon3\"}";
-=======
         return "{\"name\":\"linjhon\"}";
->>>>>>> parent of aeca78f... 1
     }
 }
