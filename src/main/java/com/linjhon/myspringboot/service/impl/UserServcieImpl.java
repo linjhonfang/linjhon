a258cfb67bc8760870c146ba226b5ed3e2cacd88
linjhonfang <linjhonfang@163.com>
2021-05-23 12:26:31 +0800

    2

--- src/main/java/com/linjhon/myspringboot/service/impl/UserServcieImpl.java
+++ src/main/java/com/linjhon/myspringboot/service/impl/UserServcieImpl.java
@@ -6,6 +6,6 @@ public class UserServcieImpl implements UserService{
 
     @Override
     public String getAllUsers() {
-        return "{\"name\":\"linjhon1\"}";
+        return "{\"name\":\"linjhon2\"}";
     }
 }
